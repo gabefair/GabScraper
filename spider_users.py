da58a55427ad1c36f4c47443a261a29842efefa6
Gabriel Fair <gabrielfair@gmail.com>
2018-08-27 19:23:06 -0400

    Won't break when attempting to read private accounts

--- spider_users.py
+++ spider_users.py
@@ -10,19 +10,24 @@ def spider():
 
 	for i in existing:
 		prefix = i[0:2].lower()
-		data_file = json.loads(open("users/" + prefix + "/" + i + "/json", "r").read())
-		for user in data_file["following"]["data"]:
-			if user["username"] not in existing and user["username"] not in new:
-				new.append(user["username"])
-		for user in data_file["followers"]["data"]:
-			if user["username"] not in existing and user["username"] not in new:
-				new.append(user["username"])
+		try:
+                        data_file = json.loads(open("users/" + prefix + "/" + i + ".json", "r").read())
+                        for user in data_file["following"]["data"]:
+                                if user["username"] not in existing and user["username"] not in new:
+                                        new.append(user["username"])
+                        for user in data_file["followers"]["data"]:
+                                if user["username"] not in existing and user["username"] not in new:
+                                        new.append(user["username"])
+                except IOError:
+                        pass
 
 	with open("names.txt", "w") as f:
 		for user in existing:
 			f.write(user)
 		for user in new:
-			f.write(new)
+                        #print user
+			f.write(user)
+			f.write("\n")
 
 	print len(new), "new users discovered."
 	print len(existing) + len(new), "total users"
